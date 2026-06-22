00ab0282dd67eee79faf5256d839d81a088b0682
ho7742@wayne.edu <ho7742@wayne.edu>
2026-06-22 19:16:40 -0400

    Build: Force fresh pip install with psycopg2-binary

--- app.py
+++ app.py
@@ -57,4 +57,4 @@ def handle_challenges():
             return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000)# Cache break 
